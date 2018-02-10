354b9e520cd1d336c530e8757787557b06b57140
Sergey Voroshilin <medulla_261@mail.ru>
2018-02-10 20:50:23 +0300

    Initial commit

---
+++ CopyrightSaver/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CopyrightSaver
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblCopyright = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblCopyright
+			// 
+			this.lblCopyright.AutoSize = true;
+			this.lblCopyright.Location = new System.Drawing.Point(24, 22);
+			this.lblCopyright.Name = "lblCopyright";
+			this.lblCopyright.Size = new System.Drawing.Size(0, 13);
+			this.lblCopyright.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(314, 161);
+			this.Controls.Add(this.lblCopyright);
+			this.MaximumSize = new System.Drawing.Size(330, 200);
+			this.MinimumSize = new System.Drawing.Size(330, 200);
+			this.Name = "MainForm";
+			this.Text = "© Copyright";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label lblCopyright;
+	}
+}
+
